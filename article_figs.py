0ff86d9add3952b3703a12cbef30f57835a63b31
Matt2371 <mtwchen@ucdavis.edu>
2022-07-14 17:35:53 -0700

    New p-value figure

--- article_figs.py
+++ article_figs.py
@@ -47,8 +47,6 @@ def plot_ensemble():
     axes[1].fill_between(df_flood['2000-10-1':'2098-10-1'].index, flood_min, flood_max,
                          facecolor='green', alpha=0.2, zorder=5)
 
-
-
     axes[0].set_ylabel('reliability')
     axes[0].set_title('Water supply reliability projections, 30 yr MA')
     axes[1].set_ylabel('flood volume (TAF)')
@@ -255,7 +253,8 @@ def plot_multi_sorted(win_size=30):
 
     return
 
-# plot p-vals of scenarios that showed a detection
+
+# plot p-vals of 50 randomly selected scenarios
 def plot_pvals(win_size=30):
     # import pvals, delete values before year 2000 (historical)
     rel_p_vals = pd.read_csv('significance_results/nonparametric/' + 'Rel_SOD_%' + '/' + str(win_size) +
@@ -290,9 +289,161 @@ def plot_pvals(win_size=30):
     return
 
 
+# plot p-vals of scenarios that showed a detection and did not (25 randomly selected from each)
+def pvals_filter(win_size=30):
+    # import pvals, delete values before year 2000 (historical)
+    rel_p_vals = pd.read_csv('significance_results/nonparametric/' + 'Rel_SOD_%' + '/' + str(win_size) +
+                             '_year_MA/' + 'less' + '_pvals_win' + str(win_size) + '.csv', index_col=0,
+                             parse_dates=True).drop(['count', 'rel_count'], axis=1)
+    flood_p_vals = pd.read_csv('significance_results/nonparametric/' + 'Upstream_Flood_Volume_taf' + '/' + str(win_size)
+                               + '_year_MA/' + 'greater' + '_pvals_win' + str(win_size) + '.csv', index_col=0,
+                               parse_dates=True).drop(['count', 'rel_count'], axis=1)
+    rel_p_vals = rel_p_vals['2000-10-1':'2098-10-1']
+    flood_p_vals = flood_p_vals['2000-10-1':'2098-10-1']
+
+    # randomly select 25 scenarios with detection
+    rel_no_detect_names = pd.read_csv('significance_results/nonparametric/Rel_SOD_%/additional_materials/no_detect_'
+                                      'scenarios_less_single_win30.csv',
+                                      index_col=0).sample(n=25, axis=0, random_state=0)['Model'].tolist()
+    flood_no_detect_names = pd.read_csv('significance_results/nonparametric/Upstream_Flood_Volume_taf/'
+                                        'additional_materials/no_detect_scenarios_greater_single_win30.csv',
+                                        index_col=0).sample(n=25, axis=0, random_state=0)['Model'].tolist()
+    rel_p_vals_no_detect = rel_p_vals[rel_no_detect_names]
+    flood_p_vals_no_detect = flood_p_vals[flood_no_detect_names]
+
+    # randomly select 25 scemarios without detection
+    rel_p_vals_detect = rel_p_vals.drop(rel_no_detect_names, axis=1).sample(n=25, axis=1, random_state=0)
+    flood_p_vals_detect = flood_p_vals.drop(flood_no_detect_names, axis=1).sample(n=25, axis=1, random_state=0)
+
+    # plot pvals
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=[10, 5], sharey=True)
+    rel_p_vals_detect.plot(ax=axes[0], c='wheat', legend=False)
+    rel_p_vals_no_detect.plot(ax=axes[0], c='lightgray', legend=False)
+    axes[0].set_ylabel('p-value')
+    axes[0].set_title('Water supply reliability')
+
+    flood_p_vals_detect.plot(ax=axes[1], c='wheat', legend=False)
+    flood_p_vals_no_detect.plot(ax=axes[1], c='lightgray', legend=False)
+    axes[1].set_title('Upstream flood volume')
+
+    # plot p=0.05
+    axes[0].axhline(y=0.05, color='r')
+    axes[1].axhline(y=0.05, color='r')
+
+    # create custom legend
+    legend_elements = [Line2D([0], [0], color='wheat', label='scenarios with detection'),
+                       Line2D([0], [0], color='lightgray', label='scenarios without detection'),
+                       Line2D([0], [0], color='red', label='p=0.05'),
+                       ]
+    axes[1].legend(handles=legend_elements, loc='upper right')
+
+    plt.tight_layout()
+    plt.savefig('significance_results/article_figures/p_vals_win' + str(win_size) + '_filtered.png')
+    plt.clf()
+
+    return
+
+
+# Single scenario: Plot first detection year against objective value (moving average) at END OF PROJECTION (2098)
+# INCLUDES DISTRIBUTION OF NO DETECT SCENAIORS' SEVERITY (BOXPLOT)
+def detect_vs_end_obj(objective, win_size=30):
+    # set alternative and figure title based on objective
+    if objective == 'Rel_SOD_%':
+        alt = 'less'
+        name = 'water supply reliability'
+    else:
+        alt = 'greater'
+        name = 'upstream flood volume'
+
+    # initiate empty lists to store data WITH DETECTION
+    model_list = []
+    detection_year = []
+    objective_values = []
+
+    # initiate empty lists to store data from NO DETECT scenarios
+    model_list_nd = []
+    objective_values_nd = []
+
+    # load csv with all first detection years
+    df_years = pd.read_csv('significance_results/nonparametric/' + objective + '/' + str(win_size) + '_year_MA/' +
+                           alt + '_single_total_' + 'win' + str(win_size) + '.csv', index_col=0)
+    # iterate over models, row stores model name and first detection year
+    for row in tqdm(df_years.itertuples(index=False), desc='Building df...'):
+        # handle no detect models
+        if np.isnan(row[1]):
+            model_list_nd.append(row[0])
+
+            # extract gcm/rcp/lulc from model name
+            gcm = row[0].split('_')[0]
+            rcp = row[0].split('_')[1]
+            try:
+                lulc = row[0].split('_')[2] + '_' + row[0].split('_')[3]
+            except IndexError:
+                lulc = row[0].split('_')[2]
+
+            # read model objective value at 2098 (trailing average)
+            scenario = gcm + '_' + rcp + '_r1i1p1'
+            df_obj = pd.read_csv('data/obj_' + scenario + '_' + lulc + '.csv.zip', index_col=0, parse_dates=True)
+            objective_values_nd.append(df_obj.loc[str(2098 - win_size + 1) + '-10-1':'2098-10-1', objective].mean())
+
+        else:
+            model_list.append(row[0])
+            detection_year.append(row[1])
+
+            # extract gcm/rcp/lulc from model name
+            gcm = row[0].split('_')[0]
+            rcp = row[0].split('_')[1]
+            try:
+                lulc = row[0].split('_')[2] + '_' + row[0].split('_')[3]
+            except IndexError:
+                lulc = row[0].split('_')[2]
+
+            # read model objective value at 2098 (trailing average)
+            scenario = gcm + '_' + rcp + '_r1i1p1'
+            df_obj = pd.read_csv('data/obj_' + scenario + '_' + lulc + '.csv.zip', index_col=0, parse_dates=True)
+            objective_values.append(df_obj.loc[str(2098 - win_size + 1) + '-10-1':'2098-10-1', objective].mean())
+
+    # build data frames and export plot ('ND', "no detection", stores severity of no detect scenarios)
+    data = {'Model': model_list, 'Detection_year': detection_year, objective: objective_values}
+    data_nd = {'Model': model_list_nd, 'ND': objective_values_nd}
+    output_df = pd.DataFrame(data)
+    output_nd_df = pd.DataFrame(data_nd)
+
+    # create scatter (right) and boxplot of values for scenarios with no detection (left)
+    fig = plt.figure()
+    gs = fig.add_gridspec(1, 2, wspace=0, width_ratios=[10, 1])
+    axs = gs.subplots(sharey=True)
+
+    output_df.plot.scatter(x='Detection_year', y=objective, ax=axs[0])
+    boxplot = output_nd_df.boxplot(column='ND', grid=False, return_type='dict', ax=axs[1], widths=0.7)
+
+    # customize plots
+    # see boxplot customization options:
+    # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.boxplot.html#matplotlib.pyplot.boxplot
+    axs[1].spines['left'].set_visible(False)
+    axs[1].tick_params(left=False)
+    axs[0].spines['right'].set_visible(False)
+    axs[0].set_xlabel('detection year')
+    axs[0].set_ylabel(name)
+    boxplot['fliers'][0].set(markersize=4, mec='black')
+    boxplot['boxes'][0].set(linewidth=1, color='black')
+    boxplot['medians'][0].set(linewidth=1, color='black')
+    for cap in boxplot['caps']:
+        cap.set(linewidth=1, color='black')
+    for whisker in boxplot['whiskers']:
+        whisker.set(linewidth=1, color='black')
+
+    plt.savefig(
+        'significance_results/article_figures/' + objective + '_detect_2098obj_' + alt + '_single_'
+        + 'win' + str(win_size) + '.svg')
+    plt.clf()
+
+    return output_df
+
+
 def main():
-    # create ensemble subplots
-    plot_ensemble()
+    # # create ensemble subplots
+    # plot_ensemble()
 
     # # plot distribution of first detection
     # plot_single_total()
@@ -309,6 +460,13 @@ def main():
 
     # # plot p-vals
     # plot_pvals(win_size=30)
+
+    # plot filtered p-vals
+    pvals_filter()
+
+    # # plot detection vs. end of simulation objective severity
+    # detect_vs_end_obj('Rel_SOD_%')
+    # detect_vs_end_obj('Upstream_Flood_Volume_taf')
 
     return
 
